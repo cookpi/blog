6fd7fe00f19f5776eae9e873a76c4dbc8dd3f0d5
mhma <mhma@cookapps.com>
2019-02-27 10:16:26 +0900

    deploy test

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,8 @@
 #!/usr/bin/env sh
 
-# abort on errors
-set -e
-
-# build
 npm run docs:build
-
-# navigate into the build output directory
 cd dist/docs
-
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
 git init
 git add -A
-git commit -m 'deploy by travis'
-
-# if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:cookappsdev/cookappsdev.github.io.git master
-
-cd -
+git commit -m 'deploy via travis'
+git push -f https://__GITHUB_TOKEN__@github.com:cookappsdev/cookappsdev.github.io.git master
