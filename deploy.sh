148d5262afcffb248a63a4d144828b7cc21fec9d
jwkim2 <jwkim2@cookapps.com>
2019-03-21 11:32:49 +0900

    merge deploy script to travis.yml

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-npm run build
-cd dist/docs
-echo "blog.cookapps.io" > CNAME
-git init
-git add -A
-git commit -m 'deploy via travis'
-git push -f https://__GITHUB_TOKEN__@github.com/cookpi/cookpi.github.io.git master
